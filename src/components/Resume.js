f8850f9fe6f19214ca4ebad4f62dc20c2bf89c98
samc995 <Sam995@gmail.com>
2023-05-25 19:42:03 -0700

    mostly finished portfolio

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState } from "react";
+// import PDF from "../assets/fake-resume.pdf";
 
 export default function Resume() {
-  return (
-    <div>
+  const [showPDF, setShowPDF] = useState(true);
 
-      <h1>Resume</h1>
-     
+  const handleClick = () => {
+    setShowPDF(!showPDF);
+  };
+
+  return (
+    <div className="m-5">
+      <div className="pb-2">
+        <h3>Resume</h3>
+        <h6>
+          Print or download my{" "}
+          <a href="#resumePDF" onClick={handleClick}>
+            resume
+          </a>
+        </h6>
+      </div>
+      {showPDF ? (
+        <div>
+          <ul>
+            Front-end Proficiencies
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>Responsive Design</li>
+            <li>ReactJS</li>
+            <li>Bootstrap</li>
+          </ul>
+          <ul>
+            Back-end Proficiencies
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL & Sequelize</li>
+            <li>MongoDB & Mongoose</li>
+            <li>REST APIs</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
+      ) : (
+        <div className="w-100">
+          {/* <object aria-label="pdf resume" width="100%" height="800" data={PDF} type="application/pdf" /> */}
+        </div>
+      )}
     </div>
   );
 }
